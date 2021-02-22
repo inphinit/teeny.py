814cacfe977a05776bbf6012b4c57a880ef3542d
Guilherme Nascimento <brcontainer@yahoo.com.br>
2021-02-21 21:34:19 -0300

    Update .gitattributes

---
+++ app.py
@@ -0,0 +1,30 @@
+# from ...teeny import Teeny
+
+from teeny import Teeny
+
+
+def test():
+    print('234')
+
+
+app = Teeny(8080)
+
+app.action('GET', '/sugar', test)
+
+
+@app.action('POST', '/sugar')
+def xyz():
+    print(12345)
+
+
+app.action('GET', '/^,$,|,[,],(,),:,<,>,!,?,#/<abc>', test)
+
+
+@app.action('GET', '/blog/<name>-<id:num>')
+def article():
+    print(12345)
+
+
+## app.exec()
+
+app.listen('GET', '/^,$,|,[,],(,),:,<,>,!,?,#/A1000')
